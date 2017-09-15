2f340c0d775acfe173ba9ad43b78b36732064457
Daniel <dasch@udel.edu>
2017-09-15 16:11:06 -0400

    I really just do not understand graphics

--- OrcMVC/src/orcMVC/OrcController.java
+++ OrcMVC/src/orcMVC/OrcController.java
@@ -1,5 +1,79 @@
 package orcMVC;
 
-public class OrcController {
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.JFrame;
+
+public class OrcController {
+	OrcModel m = new OrcModel();
+	public static void main(String[] args) {
+		/*
+		 * CARDINAL DIRECTION TO FRAME ORIENTATION North = Top of the frame
+		 * South = Bottom of the frame East = Right of the frame West = Left of
+		 * the frame
+		 */
+		OrcView v = new OrcView();
+		int xInit = 0;
+		int yInit = 0;
+
+		// v.frame.getContentPane().add();
+		// frame.getContentPane().add(new Animation(xInit, yInit,
+		// orcAction.FORWARD_SOUTHEAST));
+		v.frame.setBackground(Color.gray);
+		v.frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		v.frame.setSize(OrcModel.frameWidth, OrcModel.frameHeight);
+		v.frame.setVisible(true);
+		for (int i = 0; i < 1000; i++) {
+			v.frame.repaint();
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+	public static BufferedImage createImage(OrcModel.orcAction specifiedAction) { // orcAction is an enum corresponding to a png file
+		BufferedImage bufferedImage;
+		String file = "";
+		switch (specifiedAction) {
+		case FORWARD_NORTHEAST:
+			file = "images/orc/orc_forward_northeast.png";
+			break;
+		case FORWARD_NORTHWEST:
+			file = "images/orc/orc_forward_northwest.png";
+			break;
+		case FORWARD_SOUTHEAST:
+			file = "images/orc/orc_forward_southeast.png";
+			break;
+		case FORWARD_SOUTHWEST:
+			file = "images/orc/orc_forward_southwest.png";
+			break;
+		case FORWARD_NORTH:
+			file = "images/orc/orc_forward_north.png";
+			break;
+		case FORWARD_SOUTH:
+			file = "images/orc/orc_forward_south.png";
+			break;
+		case FORWARD_EAST:
+			file = "images/orc/orc_forward_east.png";
+			break;
+		case FORWARD_WEST:
+			file = "images/orc/orc_forward_west.png";
+			break;
+		}
+		try {
+			bufferedImage = ImageIO.read(new File(file));
+			return bufferedImage;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
+		
--- OrcMVC/src/orcMVC/OrcModel.java
+++ OrcMVC/src/orcMVC/OrcModel.java
@@ -1,9 +1,13 @@
 package orcMVC;
 
+import java.awt.image.BufferedImage;
+
 public class OrcModel {
 	//current picture that is being displayed
 	int picNum = 0;
 
+	BufferedImage[][] pics;
+	
 	final int xIncr = 8;
 	final int yIncr = 2;
 	
@@ -19,11 +23,29 @@ public class OrcModel {
 	final static int yOffset = 30;
 	
 	final int frameCount = 10;
-	
+
 	orcAction currentAction;
+	
+	public OrcModel() {
+		picNum = 0;
+	}
+	
     
     public enum orcAction {
     	FORWARD_NORTHEAST, FORWARD_NORTHWEST, FORWARD_SOUTHEAST, FORWARD_SOUTHWEST, FORWARD_NORTH, FORWARD_SOUTH, FORWARD_EAST, FORWARD_WEST
     }
-
+    
+    public void images(OrcModel m, int xStart, int yStart, OrcModel.orcAction initialAction){ //original x location, y location, and initial action
+		m.currentAction = initialAction;
+		m.xloc = xStart;
+		m.yloc = yStart;
+		BufferedImage img;
+		m.pics = new BufferedImage[orcAction.values().length][m.frameCount];
+		for (OrcModel.orcAction a : orcAction.values()) {
+			for (int i = 0; i < m.frameCount; i++) {
+				img = OrcController.createImage(a);
+				m.pics[a.ordinal()][i] = img.getSubimage(imgWidth * i, 0, imgWidth, imgHeight);
+			}
+		}
+    }
 }
